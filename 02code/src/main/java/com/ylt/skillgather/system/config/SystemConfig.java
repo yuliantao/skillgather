2e963d88e77248c743e5ccce6afcc2e0baba7c6b
yuliantao <464399631@qq.com>
2018-11-07 17:25:39 +0800

    Update 02code/src/main/java/com/ylt/skillgather/system/config/SystemConfig.java

--- 02code/src/main/java/com/ylt/skillgather/system/config/SystemConfig.java
+++ 02code/src/main/java/com/ylt/skillgather/system/config/SystemConfig.java
@@ -3,7 +3,7 @@ package com.ylt.skillgather.system.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-@MapperScan(value = "com.ylt.skillgather.system.dao")
+@MapperScan(value = "com.ylt.skillgather.system.mapper")
 @Configuration
 public class SystemConfig {
 
